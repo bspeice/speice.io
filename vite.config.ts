738e2a4e7e2ac7376358f8beef26f94e9ac96ad8
Bradlee Speice <bradlee@speice.io>
2023-04-15 12:56:22 -0400

    Run `prettier`

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import blog from "@bspeice/vite-plugin-blog"
-import mdx from "@mdx-js/rollup"
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import blog from "@bspeice/vite-plugin-blog";
+import mdx from "@mdx-js/rollup";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
     blog({
-      "/": "/pages/index"
+      "/": "/pages/index",
     }),
     mdx(),
-    react()
+    react(),
   ],
-})
+});
