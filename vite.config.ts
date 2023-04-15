3d7538bfbe6642022be70fa695ac93bc1261f7a1
Bradlee Speice <bradlee@speice.io>
2023-04-15 12:55:22 -0400

    Initial commit for new blog generator

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import blog from "@bspeice/vite-plugin-blog"
+import mdx from "@mdx-js/rollup"
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [
+    blog({
+      "/": "/pages/index"
+    }),
+    mdx(),
+    react()
+  ],
+})
