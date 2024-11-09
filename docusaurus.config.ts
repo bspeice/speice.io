853bf5ebd01d5867fc114f3d85571a4df90adf9e
Bradlee Speice <bradlee@speice.io>
2024-11-09 16:26:36 -0500

    First posts from speice.io

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -80,7 +80,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.oneLight,
       darkTheme: prismThemes.oneDark,
-      additionalLanguages: ['julia']
+      additionalLanguages: ['java', 'julia', 'nasm']
     },
   } satisfies Preset.ThemeConfig,
   plugins: [require.resolve('docusaurus-lunr-search')],
